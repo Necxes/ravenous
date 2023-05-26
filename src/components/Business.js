8101fd97da4ca47851b3ca055d5d5aef99838bee
Necxes <contact@patrick-stark.com>
2023-05-26 16:04:00 +0200

    Business styling

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,23 +1,25 @@
 import React from "react";
-import style from "./Business.css";
+import "./Business.css";
 
-export default function Business ({ image, name, address, city, state, zipcode, category, rating, review_count}) {
+export default function Business ({ imageSrc, name, address, city, state, zipcode, category, rating, reviewCount}) {
     return(
-        <article className="">
-            <img src={image} alt={name} />
-            <h4>{name}</h4>
-            <div>
-                <div>
-                    <p>{address}<br/>{city}<br />{zipcode} {state}</p>
+        <div className="col-md-4 col-6">
+            <article>
+                <img src={imageSrc} alt={name} />
+                <h4 className="text-start my-3">{name}</h4>
+                <div className="d-flex justify-content-between">
+                    <div className="text-start">
+                        <p>{address}<br/>{city}<br />{zipcode} {state}</p>
+                    </div>
+                    <div className="text-end">
+                        <p>
+                            <span className="category">{category}</span><br/>
+                            <span>{rating}</span><br/>
+                            {reviewCount}
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p>
-                        <span>{category}</span><br/>
-                        <span>{rating}</span><br/>
-                        {review_count}
-                    </p>
-                </div>
-            </div>
-        </article>
+            </article>
+        </div>
     )
 }
