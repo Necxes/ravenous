d560923560369961b4af22bdeae929408c18d815
Necxes <contact@patrick-stark.com>
2023-05-26 20:21:06 +0200

    added Header and a bit of styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import BusinessList from './components/BusinessList';
+import Header from './components/Header';
 
 const array = [1,2,3,4,5]
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <BusinessList array={array} />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="mb-5">
+            <div className="container-fluid py-3 nav">
+                <h1>ravenous</h1>
+            </div>
+            <div className="container-fluid searchContainer">
+                
+            </div>
+        </header>
+    )
+}
