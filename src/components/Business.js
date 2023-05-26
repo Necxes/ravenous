85bc1136029d9c2f3168d6927e6bb531c5dda863
Necxes <contact@patrick-stark.com>
2023-05-26 15:27:14 +0200

    added markup to business component

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,9 +1,23 @@
 import React from "react";
 import style from "./Business.css";
 
-export default function Business () {
+export default function Business ({ image, name, address, city, state, zipcode, category, rating, review_count}) {
     return(
-        <article>
+        <article className="">
+            <img src={image} alt={name} />
+            <h4>{name}</h4>
+            <div>
+                <div>
+                    <p>{address}<br/>{city}<br />{zipcode} {state}</p>
+                </div>
+                <div>
+                    <p>
+                        <span>{category}</span><br/>
+                        <span>{rating}</span><br/>
+                        {review_count}
+                    </p>
+                </div>
+            </div>
         </article>
     )
 }
